aea7305d51b155ab0a30f1623dd1e5626ac6e956
yyytir777 <yyytir777@gmail.com>
2023-01-15 03:59:31 +0900

    .

--- C++/Baekjoon/10815.cpp
+++ C++/Baekjoon/10815.cpp
@@ -1,36 +1,40 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
     
     int cnt_n;
     cin >> cnt_n;
     //cnt_n : 배열arr_n의 크기
 
-    int arr_n[cnt_n];
-    //배열 arr_n 선언(상근이의 숫자카드)
+    vector <int> vector_n;
 
     for(int i = 0; i < cnt_n; i++){
-        cin >> arr_n[i];
+        int n;
+        cin >> n;
+        vector_n.push_back(n);
     }
+    //vector_n에 입력
 
     int cnt_m;
     cin >> cnt_m;
     //cnt_m : 배열 arr_m의 크기
 
     int arr_m[cnt_m];
-    //배열 arr_m 선언(정수 M개)
 
     for(int i = 0; i < cnt_m; i++){
         cin >> arr_m[i];
+        
     }
 
-    int arr_boolean[cnt_m] = {0};
-    //arr_boolean : 상근이가 숫자카드를 가지고있는지 여부
+    //vector_m에 입력
 
+    int arr_boolean[cnt_m];
+    //arr_boolean : 상근이가 숫자카드를 가지고있는지 여부 크기 : cnt_m
+
+    /*
     for(int i = 0; i < cnt_n; i++){
         for(int j = 0; j < cnt_m; j++){
             if (arr_n[i] == arr_m[j]){
@@ -39,11 +43,25 @@ int main(){
             }
         }
     }
+    => 시간복잡도 : n^2 이므로 vector -> binary search 사용해야함
+    */
+
+    sort(vector_n.begin(), vector_n.end());
+    //상근이의 카드 정렬
+
+    for(int i = 0; i < cnt_m; i++){
+        if(binary_search(vector_n.begin(), vector_n.end(), arr_m[i])){
+            arr_boolean[i] = 1;
+        }
+        else{
+            arr_boolean[i] = 0;
+        }
+    }
+    //arr_boolean 입력
 
     for(int i = 0; i < cnt_m; i++){
         cout << arr_boolean[i] << " ";
     }
     //arr_boolean 출력
-
     return 0;
 }
--- C++/Baekjoon/1181.cpp
+++ C++/Baekjoon/1181.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <algorithm>
+#include <vector>
 using namespace std;
 
+bool compare(string a, string b){
+    if(a.size() == b.size()){ //두개의 단어의 길이가 같을때
+        return a < b; //a, b 순서대로 출력
+    }
+    else{ //다를때
+        return a.size() < b.size(); //a, b 순서대로 출력
+    }
+}
+
 int main(){
+    vector<string> v;
+    // v : 단어 입력받아넣음
+
     int n;
     cin >> n;
+    // n : 단어개수
 
+    for(int i = 0; i < n; i++){
+        string word;
+        cin >> word;
+        v.push_back(word);
+    }
+    //v에 단어 입력 완료
+
+    sort(v.begin(), v.end(), compare);
+    //정렬
+
+    /*
+    vector<int>::iterator iter;
+    //iterator : 벡터의 원소를 참조
+    for(iter = v.begin(); iter != v.end(); iter++){
+
+    }
+    */
+
+    string a;
+    for(int i = 0; i < v.size(); i++){
+        if(a == v[i]){
+            continue;
+        }
+        cout << v[i] << '\n';
+        a = v[i];
+    }
 }
