da161066f7e2abdf56d252b4e01ee89b8f833f5f
yyytir777_notebook <yyytir777@gmail.com>
2023-01-17 14:07:40 +0900

    .

--- C++/Baekjoon/1406.cpp
+++ C++/Baekjoon/1406.cpp
@@ -4,9 +4,6 @@
 using namespace std;
 
 int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
 
     list<char> editor;
     //editor char형 linked list 선언
@@ -22,44 +19,35 @@ int main(){
 
     int cnt;
     cin >> cnt;
-    string tmp;
+    char tmp;
 
     list<char>::iterator cursor = editor.end();
     //editor의 끝을 가리키는 iterator 선언 -> 커서를 표현하기 위해
 
     for(int i = 0; i < cnt; i++){
         cin >> tmp;
-        if(tmp == "L"){ 
+        if(tmp == 'L'){ 
         //커서를 왼쪽으로 한칸 옮김 & 문장의 맨 앞이면 무시
-            if(cursor == editor.begin()){
-                break;
-            }
-            else{
-                cursor--; 
+            if(cursor != editor.begin()){
+                cursor--;
             }
         }
 
-        else if(tmp == "D"){
+        else if(tmp == 'D'){
         //커서를 오른쪽으로 한칸 옮김 & 문장의 맨 뒤이면 무시
-            if(cursor == editor.end()){
-                break;
-            }
-            else{
+            if(cursor != editor.end()){
                 cursor++;
             }
         }
 
-        else if(tmp == "B"){
+        else if(tmp == 'B'){
         //커서 왼쪽에 있는 문자를 삭제 & 문장의 맨 앞이면 무시
-            if(cursor == editor.begin()){
-                break;
-            }
-            else{
-                editor.erase(--cursor);
+            if(cursor != editor.begin()){
+                cursor = editor.erase(--cursor);
             }
         }
-        
-        else if(tmp == "P"){  
+
+        else if(tmp == 'P'){  
             char add;
             cin >> add;
             editor.insert(cursor, add);
