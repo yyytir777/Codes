e86847e4bb4b0718e220815d1ac36178638b9ae4
yytir777_desktop <yyytir777@gmail.com>
2023-02-12 15:04:53 +0900

    .

---
+++ C++/Baekjoon/2579.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+#define MAX 301
+
+int stair[MAX];
+int dp[MAX];
+
+int main(){
+    int n;
+    cin >> n;
+
+    for(int i = 1; i <= n; i++){
+        cin >> stair[i];
+    } 
+
+    dp[1] = stair[1];
+    dp[2] = stair[1] + stair[2];
+    dp[3] = max(stair[1], stair[2]) + stair[3];
+
+    for(int i = 3; i <= n; i++){
+        
+    }
+
+    cout << dp[n];
+    return 0;
+}
