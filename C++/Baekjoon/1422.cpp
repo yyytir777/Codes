c49a898468959ed0a3c3ce4f2a12e044fe6ab1ec
yyytir777_notebook <yyytir777@gmail.com>
2023-03-08 20:13:20 +0900

    .

--- C++/Baekjoon/1422.cpp
+++ C++/Baekjoon/1422.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include <algorithm>
 using namespace std;
 
-vector<int> q;
+vector<string> v;
+vector<string> result;
 
-bool compare(int a, int b){
-    return a > b;
+bool compare(string a, string b){
+    if(a.length() == b.length()){
+        return a > b;
+    }
+    return a.length() > b.length();
+}
+
+bool bigger(string a, string b){
+    return (a + b) > (b + a);
 }
 
 int main(){
@@ -14,14 +23,32 @@ int main(){
     cin >> k >> n;
 
     for(int i = 0; i < k; i++){
-        int num;
+        string num;
         cin >> num;
-        q.push_back(num);
+        v.push_back(num);
     }
 
-    sort(q.begin(), q.end(), compare);
+    sort(v.begin(), v.end(), compare);
 
-    
+    for(int i = 0; i < k; i++){
+        string tmp = string(v[i]);
+        result.push_back(tmp);
+    }
 
+    for(int i = k; i < n; i++){
+        result.push_back(v[0]);
+        //가장 큰 수 입력
+    }
+    //문자를 모두 입력한 상황
+
+    sort(result.begin(), result.end(), bigger);
+    //제일 큰 수 완료...?
+
+    string a = "";
+    for(int i = 0; i < result.size(); i++){
+        a += result[i];
+    }
+
+    cout << a << '\n';
     return 0;
 }
