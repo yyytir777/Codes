bb440b1c2feb87041d02187890495651157ea255
yyytir777_notebook <yyytir777@gmail.com>
2023-03-14 01:06:49 +0900

    .

--- C++/Baekjoon/11286.cpp
+++ C++/Baekjoon/11286.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <queue>
+#include <algorithm>
 using namespace std;
+
+queue<pair<int, bool>> pq;//ture = + & false = -
+
 
 int main(){
     ios::sync_with_stdio(false);
@@ -7,7 +12,33 @@ int main(){
 
     int n;
     cin >> n;
+    for(int i = 0; i < n; i++){
+        int x;
+        cin >> x;
 
-    
+        if(x != 0){
+            if(x >= 0){
+                pq.push(make_pair(x, 0));
+            }
+            else if(x < 0){
+                pq.push(make_pair(x, 1));
+            }
+        }
+        else{ // x == 0
+            if(pq.empty()){
+                cout << "0" << '\n';
+                continue;
+            }
+            else{
+                if(pq.top().second == 0){
+                    cout << pq.top().first << '\n';
+                }
+                else if(pq.top().second == 1){
+                    cout << pq.top().first << '\n';                    
+                }
+                pq.pop();
+            }
+        }
+    }
     return 0;
 }
