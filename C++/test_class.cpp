c664af66b1628c4bf1f0aece7d93b83e9f0fbac6
yyytir777_notebook <yyytir777@gmail.com>
2023-04-06 01:14:10 +0900

    .

---
+++ C++/test_class.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class A{
+private:
+    int floor;
+    
+public:`
+    A();
+
+    A(int s){
+        floor = s;
+    }
+
+    int getfloor(){
+        return floor;
+    }
+};
+
+int main(){
+    A a(3);
+    A b();
+}
