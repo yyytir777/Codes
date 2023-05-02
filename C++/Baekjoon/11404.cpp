1e28dc7c2e3dbb4eb4253f149185214aeca77ef6
yyytir777_notebook <yyytir777@gmail.com>
2023-05-02 11:56:52 +0900

    .

--- C++/Baekjoon/11404.cpp
+++ C++/Baekjoon/11404.cpp
@@ -1,29 +1,48 @@
 #include <iostream>
 using namespace std;
 
-int d[101][101];  // i에서 j까지 가는 최단거리
+#define INF 987654321 // <-- 가중치와 차이가 많이 나는 정수여야함
+#define MAX 101
+int w[MAX][MAX] = {0,};  // i에서 j까지 가는 최단거리
 
 int main(){
     int n, m;
     cin >> n >> m;
 
     for(int i = 1; i <= m; i++){
-        int x, y, cost;
+        int x, y, cost; //시작도시, 도착도시, 비용
         cin >> x >> y >> cost;
-        d[x][y] = cost;
+        if(w[x][y] == 0 || w[x][y] > cost)
+            w[x][y] = cost;
+    }
+
+    int d[MAX][MAX];
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= n; j++){
+            if(i == j)
+                d[i][j] = 0;
+            else if(w[i][j]) //w[i][j]가 0이 아닐때
+                 d[i][j] = w[i][j];
+            else
+                d[i][j] = INF;
+        }
     }
 
     for(int k = 1; k <= n; k++){
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
-                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+                if(d[i][j] > d[i][k] + d[k][j])
+                    d[i][j] = d[i][k] + d[k][j];
             }
         }
     }
 
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
-            cout << d[i][j] << " ";
+            if(i == j || d[i][j] == INF)
+                cout << 0 << " ";
+            else
+                cout << d[i][j] << " ";
         }
         cout << '\n';
     }
