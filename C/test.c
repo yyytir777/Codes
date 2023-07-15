8eb5cc689cb0dc4a71ee3090d31682759e94c8c7
yytir777_desktop <yyytir777@gmail.com>
2023-07-16 03:01:53 +0900

    .

--- C/test.c
+++ C/test.c
@@ -1,14 +1,6 @@
 #include <stdio.h>
-#define PI 3.14
-
-double GetArea(double radius){
-    return PI * radius * radius;
-}
 
 int main(){
-    double r;
-    printf("원의 반지름을 입력하시오 : ");
-    scanf("%lf", &r);
-
-    printf("원의 면적은 %lf입니다.\n", GetArea(r));
+    int len = 4;
+    int arr[len];
 }
