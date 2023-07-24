33c217a7362169416ec3a9260b5b4d4cdc8688f8
yyytir777_notebook <yyytir777@gmail.com>
2023-07-24 16:51:17 +0900

    .

--- C/test1.c
+++ C/test1.c
@@ -1,29 +1,7 @@
 #include <stdio.h>
 
-int arr[1000];
-float arr1[1000];
-
 int main(){
-    int n;
-    scanf("%d", &n);
-
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
-
-    int max = 0;
-    for (int i = 0; i < n; i++)
-    {
-        if(arr[i] > max)
-            max = arr[i];
-    }
-    
-    float sum = 0;
-    for(int i = 0; i < n; i++){
-        sum += (float)arr[i] / max * 100;
-    }
-
-    printf("%f\n", sum / n);
-    return 0;
+    int arr[4] = {1,2,3,4};
+    printf("%p", arr);
+    printf("%p", &arr[0]);
 }
