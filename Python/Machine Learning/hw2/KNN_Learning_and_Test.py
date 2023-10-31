1a4ebb8138e5d7678e1e0b18d6c2c8e8ca94f3e6
yyytir777_notebook <yyytir777@gmail.com>
2023-11-01 03:34:23 +0900

    .

--- Python/Machine Learning/hw2/KNN_Learning_and_Test.py
+++ Python/Machine Learning/hw2/KNN_Learning_and_Test.py
@@ -11,5 +11,12 @@ from KNN import KNN
 (x_train, y_train), (x_test, y_test) = load_mnist(flatten=True, normalize=False)
 
 # KNN 실행
-clf1 = KNN(x_train / 255, y_train, x_test / 255, y_test, k = 3)
+clf1 = KNN(x_train, y_train, x_test, y_test, k = 15)
 clf1.run()
+
+# x_train, x_test -> x_train/255, x_test/255와 같이 정규화를
+# 하냐 안하느냐에 따라 달라지므로 이를 확인할것.
+
+# for i in range(1, 30, 2):
+#     test_model = KNN(x_train, y_train, x_test, y_test, k = i)
+#     test_model.run()
