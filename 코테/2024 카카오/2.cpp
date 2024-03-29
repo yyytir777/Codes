22ffd037891818b5001677702030d141e18c66c0
임원재 <80938176+yyytir777@users.noreply.github.com>
2024-03-30 01:16:13 +0900

    수정

---
+++ 코테/2024 카카오/2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    double a = 0.1;
+    double b = 0.2;
+    double c = 0.3;
+    bool temp = (a + b == c);
+    cout << temp << endl;
+    return 0;
+}
