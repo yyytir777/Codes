9ecd1ab85f28e8dd5300ee4b62476090d2da7994
yyytir777 <yyytir777@gmail.com>
2024-06-09 18:46:45 +0900

    6/9

--- C++/Baekjoon/1629.cpp
+++ C++/Baekjoon/1629.cpp
@@ -1,20 +1,28 @@
 #include <bits/stdc++.h>
 using namespace std;
-int a, b, c;
+long long a, b, c;
 
-long long multiply(int a, int b, int c) {
-    if (b==1) return a % c;
-    return (a * multiply(a, b-1, c)) % c;
+long long multiply(long long i, long long j, long long k) {
+    long long result;
+    if (j==1) return i % k;
+    
+    int tmp;
+    // 짝수일 때
+    if(j % 2 == 0) {
+        tmp = multiply(i, j/2, k) % k;
+        result = tmp * tmp;
+    }
+    // 홀수일 때
+    else {
+        int mid = j / 2;
+        tmp = multiply(i, mid, k) % k;
+        result = (i % k) * (tmp * tmp % k);
+    }
+    
+    return result % k;
 }
 
 int main() {
     cin >> a >> b >> c;
-
-    for(int i = 1; i <= b; i++) {
-        a *= a;
-        a %= c;
-    }
-    cout << a;
-    // cout << multiply(a, b, c);
-    return 0;
+    cout << multiply(a, b, c);
 }
