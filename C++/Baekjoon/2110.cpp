47249cf06575dd0785798d726269c7db3b72bd39
Wonjae Lim <yyytir777@gmail.com>
2025-05-09 12:18:18 +0900

    5/9

--- C++/Baekjoon/2110.cpp
+++ C++/Baekjoon/2110.cpp
@@ -1,28 +1,53 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
+// 2110 cpp
+#include <bits/stdc++.h>
 using namespace std;
 
-long long home[1000000000] = {0,};
-int main(){
-    int n, c;
-    cin >> n >> c; // n = 집 개수, c = 공유기 개수
+int n, c;
+vector<int> houses;
 
-    for(int i = 0; i < n; i++){
-        cin >> home[i];
+void input() {
+  cin >> n >> c;
+
+  int tmp;
+  for(int i = 0; i < n; i++) {
+    cin >>tmp;
+    houses.push_back(tmp);
+  }
+
+  sort(houses.begin(), houses.end());
+}
+
+void solve() {
+  int min_dist = 1;
+  int max_dist = houses[n-1] - houses[0];
+  int result = 0;
+
+  while(min_dist <= max_dist) {
+    int start_router = houses[0];
+    int mid_dist = (min_dist + max_dist) / 2;
+
+    int router_cnt = 1;
+    for(int i = 1; i < n; i++) {
+      if(houses[i] - start_router >= mid_dist) {
+        router_cnt++;
+        start_router = houses[i];
+      }
     }
 
-    sort(home, home + n);
-
-    int min = home[0];
-    int max = home[n-1];
-    
-    while(min <= max){
-        int mid = (min + max) / 2;
-
-        for(int i = 0; i < n; i++){
-            
-        }
+    if(router_cnt < c) {
+      max_dist = mid_dist - 1;
     }
-    return 0;
+    else {
+      result = mid_dist;
+      min_dist = mid_dist + 1;
+    }
+  }
+
+  cout << result;
+}
+
+int main() {
+  input();
+  solve();
+  return 0;
 }
--- test.cpp
+++ test.cpp
@@ -1,50 +1,53 @@
-// 4358 cpp
+// 2110 cpp
 #include <bits/stdc++.h>
 using namespace std;
 
-int cnt = 0;
-map<string, int> trees;
-vector<string> treeName;
-map<string, float> ratios;
+int n, c;
+vector<int> houses;
 
 void input() {
-  string tmp;
+  cin >> n >> c;
 
-  while(true) {
-    getline(cin, tmp);
-    if(tmp.empty()) break;
-    cnt++;
-    if(trees.find(tmp) == trees.end()) {
-      trees.insert({tmp, 1});
-    } else {
-      trees[tmp]++;
-    }
+  int tmp;
+  for(int i = 0; i < n; i++) {
+    cin >>tmp;
+    houses.push_back(tmp);
   }
+
+  sort(houses.begin(), houses.end());
 }
 
 void solve() {
-  for(auto tree : trees) {
-    float ratio = (float) tree.second / cnt * 100;
-    ratios.insert({tree.first, ratio});
+  int min_dist = 1;
+  int max_dist = houses[n-1] - houses[0];
+  int result = 0;
+
+  while(min_dist <= max_dist) {
+    int start_router = houses[0];
+    int mid_dist = (min_dist + max_dist) / 2;
+
+    int router_cnt = 1;
+    for(int i = 1; i < n; i++) {
+      if(houses[i] - start_router >= mid_dist) {
+        router_cnt++;
+        start_router = houses[i];
+      }
+    }
+
+    if(router_cnt < c) {
+      max_dist = mid_dist - 1;
+    }
+    else {
+      result = mid_dist;
+      min_dist = mid_dist + 1;
+    }
   }
 
-  sort(treeName.begin(), treeName.end());
-
-  for(auto ratio : ratios) {
-    cout << ratio.first;
-    printf(" %0.4f\n", ratio.second);
-  }
-}
-
-void print() {
-  for(auto tree : trees) {
-    cout << tree.first << " : " << tree.second << "\n";
-  }
+  cout << result;
 }
 
 int main() {
   input();
   solve();
-  // print();
   return 0;
 }
