859e4e541278fdbb6045cfc1aa2ebc832cdd61a5
Wonjae Lim <yyytir777@gmail.com>
2025-05-23 11:30:11 +0900

    5/23

--- test.cpp
+++ test.cpp
@@ -1,55 +1,37 @@
-// 13904 cpp
+// 2346 cpp
 #include <bits/stdc++.h>
-#define MAX 1001
 using namespace std;
 
-typedef pair<int, int> Pair;
-
 int n;
-int t[MAX] = {0,};
-vector<Pair> v;
+deque<pair<int, int>> balloon;
 
 void input() {
 	cin >> n;
-
-	int d, w;
-	for(int i = 0; i < n; i++) {
-		cin >> d >> w;
-		v.push_back({d, w});
-	}
-}
-
-bool compare(Pair a, Pair b) {
-	if(a.second == b.second) return a.first > b.first;
-	else return a.second > b.second;
-}
-
-void print_v() {
-	for(auto node : v) {
-		printf("%d %d\n", node.first, node.second);
+	int tmp;
+	for(int i = 1; i <= n; i++) {
+		cin >> tmp;
+		balloon.push_back({tmp, i});
 	}
 }
 
 void solve() {
-	sort(v.begin(), v.end(), compare);
+	while(!balloon.empty()) {
+		pair<int, int> node = balloon.front();
+		balloon.pop_front();
+		cout << node.second << " ";
 
-	// print_v();
-
-	for(int i = 0; i < n; i++) {
-		for(int j = v[i].first; j > 0; j--) {
-			if(t[j] == 0) {
-				t[j] = v[i].second;
-				break;
+		if(node.first > 0) {
+			for(int i = 1; i < node.first && !balloon.empty(); i++) {
+				balloon.push_back(balloon.front());
+				balloon.pop_front();
+			}
+		} else {
+			for(int i = 0; i < -node.first && !balloon.empty(); i++) {
+				balloon.push_front(balloon.back());
+				balloon.pop_back();	
 			}
 		}
 	}
-
-	// for(int i = 1; i <= 1000; i++) printf("%d\n", t[i]);
-	int sum = 0;
-	for(int i = 0; i <= 1000; i++) {
-		sum += t[i];
-	}
-	cout << sum;
 }
 
 int main() {
