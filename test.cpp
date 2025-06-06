d28cfdc26b4c5810c614840cbfed3d2a7b6f8401
Wonjae Lim <yyytir777@gmail.com>
2025-06-07 00:14:29 +0900

    6/7

--- test.cpp
+++ test.cpp
@@ -1,120 +1,66 @@
-// 2638 cpp
+// 1325 cpp
 #include <bits/stdc++.h>
-#define BLANK 2
-#define CHEEZE 1
-#define HOLE 0
-#define MAX 101
+#define MAX 10001
 using namespace std;
 
-typedef pair<int, int> Pair;
-
 int n, m;
-int graph[MAX][MAX];
-bool visited[MAX][MAX] = {0,};
-queue<Pair> q;
-vector<Pair> cheezes;
-
-int r_axis[4] = {0, 0, 1, -1};
-int c_axis[4] = {1, -1, 0, 0};
+vector<int> parent[MAX];
+queue<int> q;
+bool visited[MAX] = {0,};
 
 void input() {
   cin >> n >> m;
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < m; j++) {
-      cin >> graph[i][j];
-    }
-  }
-}
 
-bool isEmpty() {
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < m; j++) {
-      if(graph[i][j] == CHEEZE) return false;
-    }
-  }
-  return true;
-}
-
-void bfs(Pair start) {
-  q.push(start);
-  graph[start.first][start.second] = BLANK;
-  visited[start.first][start.second] = 1;
-
-  while(!q.empty()) {
-    Pair cur_node = q.front();
-    q.pop();
-
-    for(int i = 0; i < 4; i++) {
-      int nr = cur_node.first + r_axis[i];
-      int nc = cur_node.second + c_axis[i];
-
-      if(nr < 0 || nr > n || nc < 0 || nc > m) continue;
-      if(visited[nr][nc]) continue;
-      if(graph[nr][nc] == CHEEZE) continue;
-      if(graph[nr][nc] == HOLE) graph[nr][nc] = BLANK;
-      if(graph[nr][nc] == BLANK) {
-        q.push({nr, nc});
-        visited[nr][nc] = 1;
-      }
-    }
+  int a, b;
+  for(int i = 0; i < m; i++) {
+    cin >> a >> b;
+    parent[b].push_back(a);
   }
 }
 
 void init() {
-  cheezes.clear();
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < m; j++) {
-      visited[i][j] = 0;
+  for(int i = 1; i <= n; i++) visited[i] = 0;
+}
+
+int bfs(int start) {
+  int cnt = 0;
+  q.push(start);
+  visited[start] = 1;
+
+  while(!q.empty()) {
+    int cur_node = q.front();
+    q.pop();
+
+    for(int i = 0; i < parent[cur_node].size(); i++) {
+      int next_node = parent[cur_node][i];
+      if(visited[next_node]) continue;
+      q.push(next_node);
+      visited[next_node] = 1;
+      cnt++;
     }
   }
-}
-
-void checkBlank() {
-  bfs({0,0});
-}
-
-void findFreezeCheeze() {
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < m; j++) {
-
-      int cnt = 0, ni, nj;
-      for(int k = 0; k < 4; k++) {
-        ni = i + r_axis[k];
-        nj = j + c_axis[k];
-        if(ni < 0 || ni > n || nj < 0 || nj > m) continue;
-        if(graph[ni][nj] == BLANK) cnt++;
-      }
-
-      if(cnt >= 2) cheezes.push_back({i,j});
-    }
-  }
-}
-
-void freezeCheeze() {
-  for(auto node : cheezes) {
-    graph[node.first][node.second] = BLANK;
-  }
-}
-
-void print() {
-  for(int i = 0; i < n; i++) {
-    for(int j = 0; j < m; j++) {
-      cout << graph[i][j] << " ";
-    }
-    cout << '\n';
-  }
+  return cnt;
 }
 
 void solve() {
-  int time = 0;
-  while(!isEmpty()) {
-    checkBlank(); // 외부 공기를 2로 변경
+  int max_result = 0;
+  vector<int> results;
+  for(int i = 0; i < n; i++) {
     init();
-    findFreezeCheeze(); // 녹을 예정의 치즈를 선택
-    freezeCheeze(); // 치즈를 녹임
-    time++;
+    int cnt = bfs(i+1);
+    if(max_result < cnt) {
+      max_result = cnt;
+      results.clear();
+      results.push_back(i+1);
+    }
+    else if(max_result == cnt) {
+      results.push_back(i+1);
+    }
   }
-  cout << time;
+
+  for(auto node : results) {
+    cout << node << '\n';
+  }
 }
 
 int main() {
