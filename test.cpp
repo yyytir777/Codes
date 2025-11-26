60e906e562463d0fb37f43d0633e23dd46f8258a
Wonjae Lim <yyytir777@gmail.com>
2025-11-26 13:34:44 +0900

    11/26

--- test.cpp
+++ test.cpp
@@ -1,37 +1,55 @@
+// 1013 cpp
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-vector<string> files;
-map<string, int> dicts;
+int tc;
+string wave;
 
-void input() {
-  cin >> n;
-  string tmp;
-  for(int i = 0; i < n; i++) {
-    cin >> tmp;
-    files.push_back(tmp);
-  }
-}
+/*
 
+NO
+NO
+
+*/
 void solve() {
-  string ex;
-  for(string file : files) {
-    for(int i = 0; i < file.size(); i++) {
-      if(file[i] == '.') {
-        ex = file.substr(i + 1, file.size());
-        dicts[ex] += 1;
-      }
+  int status = wave[0] - '0';
+  for(int i = 1; i < wave.length(); i++) {
+    if(status == 6) {
+      cout << "NO\n";
+      return;
+    }
+
+    if(wave[i] == '0') {
+      if(status == 0) status = 6;
+      else if(status == 1) status = 2;
+      else if(status == 2) status = 4;
+      else if(status == 3) status = 0;
+      else if(status == 4) status = 4;
+      else if(status == 5) status = 0;
+      else if(status == 7) status = 8;
+      else if(status == 8) status = 4;
+    }
+    else { // wave[i] == '1'
+      if(status == 0) status = 3;
+      else if(status == 1) status = 6;
+      else if(status == 2) status = 6;
+      else if(status == 3) status = 1;
+      else if(status == 4) status = 5;
+      else if(status == 5) status = 7;
+      else if(status == 7) status = 7;
+      else if(status == 8) status = 3;
     }
   }
 
-  for(auto dict : dicts) {
-    cout << dict.first << " " << dict.second << '\n';
-  }
+  // cout << "YES\n";
+  if(status == 3 || status == 5 || status == 7) cout << "YES\n";
+  else cout << "NO\n";
 }
 
 int main() {
-  input();
-  solve();
-  return 0;
+  cin >> tc;
+  while(tc--) {
+    cin >> wave;
+    solve();  
+  }
 }
