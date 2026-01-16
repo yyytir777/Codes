e14ca12c15d4d8cfce9fb33bb599cf9ef5a25504
Wonjae Lim <yyytir777@gmail.com>
2026-01-16 14:29:58 +0900

    1/16

--- test.cpp
+++ test.cpp
@@ -1,120 +1,145 @@
-// 1584 cpp
+// 14497 cpp
 #include <bits/stdc++.h>
-#define INF 987654321
-#define MAX 501
-#define SAFE 0
-#define RISK 1
-#define DEATH INF
+#define MAX 301
+#define BLANK 0
+#define WALL 1
+#define TEMP 2
+#define ME '*'
+#define CHOCOBAR '#'
 using namespace std;
 
 typedef pair<int, int> Pair;
-
-int graph[MAX][MAX] = {0,};
-bool visited[MAX][MAX] = {0,};
-priority_queue<pair<int, Pair>> pq;
-
 int n, m;
-int dist[MAX][MAX];
+int graph[MAX][MAX];
+bool visited[MAX][MAX] = {0,};
 
-int r_axis[4] = {1, -1, 0, 0};
-int c_axis[4] = {0, 0, 1, -1};
-
-void input() {
-  cin >> n;
-  int x1, y1, x2, y2;
-  for(int i = 0; i < n; i++) {
-    cin >> x1 >> y1 >> x2 >> y2;
-
-    for(int i = min(x1, x2); i <= max(x1, x2); i++) {
-      for(int j = min(y1, y2); j <= max(y1, y2); j++) {
-        graph[i][j] = RISK;
-      }
-    }
-  }
-
-  cin >> m;
-  for(int i = 0; i < m; i++) {
-    cin >> x1 >> y1 >> x2 >> y2;
-
-    for(int i = min(x1, x2); i <= max(x1, x2); i++) {
-      for(int j = min(y1, y2); j <= max(y1, y2); j++) {
-        graph[i][j] = DEATH;
-      }
-    }
-  }
-}
+pair<int, int> chocobar;
+pair<int, int> me;
 
 void init() {
-  for(int i = 0; i < MAX; i++) {
-    for(int j = 0; j < MAX; j++) {
-      dist[i][j] = INF;
+  for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= m; j++) {
+      visited[i][j] = 0;
     }
   }
 }
 
-bool isRisk(Pair prev, Pair cur) {
-  if(graph[prev.first][prev.second] == SAFE &&
-    graph[cur.first][cur.second] == RISK) {
-    return true;
+void input() {
+  cin >> n >> m;
+
+  cin >> me.first >> me.second;
+  cin >> chocobar.first >> chocobar.second;
+
+  string tmp;
+  for(int i = 0; i < n; i++) {
+    cin >> tmp;
+    for(int j = 0; j < m; j++) {
+      if(tmp[j] == ME) graph[i+1][j+1]= BLANK;
+      else if(tmp[j] == CHOCOBAR) graph[i+1][j+1] = BLANK;
+      else graph[i+1][j+1] = tmp[j] - '0';
+    }
   }
-  return false;
 }
 
-bool isDeath(Pair cur) {
-  if(graph[cur.first][cur.second] == DEATH) {
-    return true;
-  }
-  return false;
-}
+int r_axis[4] = {0, 0, 1, -1};
+int c_axis[4] = {1, -1, 0, 0};
 
+bool isConnected() {
+  init();
+  Pair start = me;
+  queue<Pair> q;
 
-void dijkstra() {
-  Pair start = {0,0};
+  q.push(start);
+  visited[start.first][start.second] = 1;
 
-  pq.push({0, start});
+  while(!q.empty()) {
+    Pair cur_node = q.front();
+    q.pop();
 
-
-  while(!pq.empty()) {
-    pair<int, Pair> node = pq.top();
-    pq.pop();
-
-    int cur_dist = -node.first;
-    Pair cur_node = node.second;
-
-    if(cur_node.first == 500 && cur_node.second == 500) {
-      return;
+    if(cur_node.first == chocobar.first && cur_node.second == chocobar.second) {
+      return true;
     }
 
     for(int i = 0; i < 4; i++) {
       int nxt_r = cur_node.first + r_axis[i];
       int nxt_c = cur_node.second + c_axis[i];
-      int nxt_dist = cur_dist + graph[nxt_r][nxt_c];
 
-      if(nxt_c < 0 || nxt_c >= MAX || nxt_r < 0 || nxt_r >= MAX) continue;
-
-      if(isDeath({nxt_r, nxt_c})) continue;
+      if(nxt_r <= 0 || nxt_r > n || nxt_c <= 0 || nxt_c > m) continue;
 
       if(visited[nxt_r][nxt_c]) continue;
 
-      if(nxt_dist > dist[nxt_r][nxt_c]) continue;
+      if(graph[nxt_r][nxt_c] == WALL) continue;
 
-      dist[nxt_r][nxt_c] = nxt_dist;
-      pq.push({-nxt_dist, {nxt_r, nxt_c}});
-      visited[nxt_r][nxt_c] = 1;
+      if(graph[nxt_r][nxt_c] == BLANK) {
+        visited[nxt_r][nxt_c] = 1;
+        q.push({nxt_r,nxt_c});
+      }
+    }
+  }
+
+  return false;
+} 
+
+void wave() {
+  init();
+  Pair start = me;
+  queue<Pair> q;
+
+  q.push(start);
+  visited[start.first][start.second] = 1;
+
+  while(!q.empty()) {
+    Pair cur_node = q.front();
+    q.pop();
+
+    for(int i = 0; i < 4; i++) {
+      int nxt_r = cur_node.first + r_axis[i];
+      int nxt_c = cur_node.second + c_axis[i];
+
+      if(nxt_r <= 0 || nxt_r > n || nxt_c <= 0 || nxt_c > m) continue;
+
+      if(visited[nxt_r][nxt_c]) continue;
+
+      if(graph[nxt_r][nxt_c] == WALL) {
+        graph[nxt_r][nxt_c] = TEMP;
+      }
+
+      if(graph[nxt_r][nxt_c] == BLANK) {
+        visited[nxt_r][nxt_c] = 1;
+        q.push({nxt_r,nxt_c});
+      }
     }
   }
 }
 
-void solve() {
-  init();
-  dijkstra();
-
-  if(dist[500][500] == INF) {
-    cout << "-1";
-  } else {
-    cout << dist[500][500];
+void convertTempToBlank() {
+  for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= m; j++) {
+      if(graph[i][j] == TEMP) graph[i][j] = BLANK;
+    }
   }
 }
+
+void print() {
+  for(int i = 1; i <= n; i++) {
+    for(int j = 1; j <= m; j++) {
+      cout << graph[i][j] << " ";
+    }
+    cout << '\n';
+  }
+}
+
+void solve() {
+  int cnt = 0;
+
+  while(!isConnected()) {
+    wave();
+    convertTempToBlank();
+    cnt++;
+  }
+  cout << cnt + 1;
+}
+
 
 int main() {
   input();
